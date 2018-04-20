f1aa4e3c9b163a30e41eb0f87ec429d96941a3e8
NadeekaSooriyabandara <smndsbandara@gmail.com>
2018-04-20 07:51:22 +0530

    setup angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
