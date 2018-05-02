7fe77a0f74a7db2982320af021620a70645ba54d
NadeekaSooriyabandara <smndsbandara@gmail.com>
2018-05-02 15:04:39 +0530

    add firebaseConfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC5_3qN17U4GB0CokOHe4Lkql3GWcPK3SQ',
+    authDomain: 'vehiclerequest-de1e4.firebaseapp.com',
+    databaseURL: 'https://vehiclerequest-de1e4.firebaseio.com',
+    projectId: 'vehiclerequest-de1e4',
+    storageBucket: 'vehiclerequest-de1e4.appspot.com',
+    messagingSenderId: '52933539315'
+  }
 };
